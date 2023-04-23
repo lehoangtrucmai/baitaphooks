ea23cacf357e8f03993ca548e9aa59da453ba622
LÊ HOÀNG TRÚC MAI <125021302+lehoangtrucmai@users.noreply.github.com>
2023-04-23 13:09:52 +0900

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+function index() {
+    const [products, setProducts] = useState([
+        { id: 1, name: 'Product 1', price: 10 },
+        { id: 2, name: 'Product 2', price: 20 },
+        { id: 3, name: 'Product 3', price: 30 },
+      ]);
+    
+  return (
+    <ul>
+        {products.map((product) => (
+             <li key={product.id}>
+                {product.name} - ${product.price}
+             </li>
+         ))}
+       </ul>
+  );
+}
+
+export default index
